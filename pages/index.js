cc13dff0ed73babe0c8b5fbab3a2a6de1b780882
yuanjingya@chanjet.com <yuanjingya@chanjet.com>
2021-09-15 22:27:03 +0800

    feat: init

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default () => (
+    <div>
+        <nav>
+            <Link href='/about'><a>About</a></Link>
+        </nav>
+        <p>welcome to next.js</p>
+    </div>
+)
