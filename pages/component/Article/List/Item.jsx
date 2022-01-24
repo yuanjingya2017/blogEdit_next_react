439e10d3e841df522c70bb586a0e9cc3b5e0b31a
yuanjingya2017 <17664051304@163.com>
2022-01-24 09:49:33 +0800

    文章详情页

--- pages/component/Article/List/Item.jsx
+++ pages/component/Article/List/Item.jsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
-import itemStyle from './Item.module.scss';
+import itemStyle from './item.module.scss';
+import Link from 'next/link';
 export const Item = (props) => {
-    console.log(props, '===props')
     const {
         name,
-        link,
+        id,
         time,
         comment
     } = props;
-    return <a className={itemStyle.article_item} href={link}>
-        <span className={itemStyle.title}>{name}</span>
-        <div className={itemStyle.right}>
-            <span>{time}</span>
-            <span>{comment}条评论</span>
-        </div>
-    </a>
+    return <Link key={id} href={`/detail?articleId=${id}`}>
+        <a className={itemStyle.article_item}>
+            <span className={itemStyle.title}>{name}</span>
+            <div className={itemStyle.right}>
+                <span>{time}</span>
+                <span>{comment}条评论</span>
+            </div>
+        </a>
+    </Link>
 }
