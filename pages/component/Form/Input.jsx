fda6f1cc1cc21ca24e6d26bbbe5a3a18fbf0e51a
yuanjingya2017 <17664051304@163.com>
2022-01-27 14:50:43 +0800

    textarea id

--- pages/component/Form/Input.jsx
+++ pages/component/Form/Input.jsx
@@ -1,9 +1,9 @@
 import formStyle from './form.module.scss';
 
-export const Input = ({onChange, value, type = 'text', name}) => {
+export const Input = ({onChange, value, type = 'text', name, id = ''}) => {
     return type == 'text' ? <input className={formStyle.item_input} value={value} onChange={(e) => {
         onChange && onChange(e.target.value)
-    }} /> : <textarea className={formStyle.item_input} name={name} id={name} cols="30" rows="10" value={value} onChange={(e) => {
+    }} /> : <textarea id={id} className={formStyle.item_input} name={name} id={name} cols="30" rows="10" value={value} onChange={(e) => {
         onChange && onChange(e.target.value)
     }}></textarea>
 }
